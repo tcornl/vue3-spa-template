f14d34bdbaa304b9c5d8c95282cfea163ef65d38
Tom Cornell <30247107+tcornl@users.noreply.github.com>
2022-04-11 17:09:05 -0400

    Added a Page Not Found view.

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1>Sorry, this page does not exist!</h1>
+    <button>
+      <RouterLink :to="{ name: 'home' }">Back To Home</RouterLink>
+    </button>
+  </div>
+</template>
