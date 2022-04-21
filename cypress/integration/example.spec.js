d4f8f5b07e9706fbf2562a5d071da4d50de432f7
Tom Cornell <30247107+tcornl@users.noreply.github.com>
2022-04-21 13:43:07 -0400

    Added environment variables for ease of configuration.

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -10,29 +10,31 @@ describe("My First Test", () => {
 describe("App Navigation", () => {
   it("navigates the app views and verifies page content", () => {
     cy.visit("/");
-    cy.url().should("eq", "http://localhost:5050/vue3-spa-template/#/");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/");
     cy.contains("h1", "You did it!").should("be.visible");
     cy.contains("a", "About").click();
-    cy.url().should("eq", "http://localhost:5050/vue3-spa-template/#/about");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/about");
     cy.contains("h1", "This is an about page").should("be.visible");
     cy.contains("a", "Sentry").click();
-    cy.url().should("eq", "http://localhost:5050/vue3-spa-template/#/sentry");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/sentry");
     cy.contains("button", "Throw error").should("be.visible");
   });
 });
 
 describe("Dynamic Route Matching", () => {
   it("directly navigates to non-home pages", () => {
-    cy.visit("/#/about");
-    cy.url().should("eq", "http://localhost:5050/vue3-spa-template/#/about");
+    cy.visit("#/about");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/about");
+    cy.visit("#/sentry");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/sentry");
   });
 });
 
 describe("Page Not Found", () => {
   it("indicates the page does not exist and provides a button link to home ", () => {
-    cy.visit("/#/abc123");
+    cy.visit("#/abc123");
     cy.contains("h1", "Sorry, this page does not exist!").should("be.visible");
     cy.contains("a", "Back To Home").click();
-    cy.url().should("eq", "http://localhost:5050/vue3-spa-template/#/");
+    cy.url().should("eq", Cypress.config().baseUrl + "#/");
   });
 });
